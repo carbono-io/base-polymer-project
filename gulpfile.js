02718ef31dc63bf472b6418185fbd1a3001b4a47
Simon <simonxd@gmail.com>
2015-09-27 16:40:04 -0300

    serve source files task

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+/**
+ * Task for serving resources at development time.
+ */
+gulp.task('serve:src', function () {
+    var bs = browserSync({
+        server: {
+            baseDir: 'src',
+        },
+        open: true,
+    });
+});
+
+/**
+ * Development environment setup
+ */
+gulp.task('develop', ['serve:src']);
